ba3df136e427c5b08fa89c05c272509dc620d3a7
Bikramjit <bikramjit.garcha@outlook.com>
2018-11-01 15:41:18 -0700

    fixed some errors

--- q1.c
+++ q1.c
@@ -7,9 +7,9 @@ int main (int argc, char *argv[]){
 	int inputArr[argc - 1];
 	
 
-	for(int x = 0; x< argc; x++){
-		inputArr[x] = argv[x];
-		printf["%d \n", inputArr[x]];
+	for(int x = 1; x< argc; x++){
+		inputArr[x] = atoi(argv[x]);
+		printf("%d \n", inputArr[x]);
 	}
 	return 0;
 }
