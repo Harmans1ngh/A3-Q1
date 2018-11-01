0e956504e3f91190fa10e048b09911ef1b940f9f
Harmans1ngh <Harmans1ngh@users.noreply.github.com>
2018-11-01 19:06:25 -0400

    created threads

--- q1.c
+++ q1.c
@@ -4,12 +4,23 @@
 
 int main (int argc, char *argv[]){
 
+	if(argc < 2){
+		printf("sorry sir/maam this program cannot execute due to the fact that there are not enough arguments.\n
+			please I request that you next time enter in some numbers as an argument so this program can execute with joy.\n
+			Thanks! and have a nice day.");
+	}
 	int inputArr[argc - 1];
+	int min, max, avg;
 	
 
 	for(int x = 1; x< argc; x++){
 		inputArr[x] = atoi(argv[x]);
 		printf("%d \n", inputArr[x]);
 	}
+
+	pthread_t maxThread, minThread, avgThread;
+	pthread_create(&maxThread, NULL, findMax, inputArr);
+	pthread_create(&minThread, NULL, findMin, inputArr);
+	pthread_create(&avgThread, NULL, findAvg, inputArr);
 	return 0;
 }
