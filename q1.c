fcf13dabf7f6e0273d4c38956e2dc675c3b6706b
Bikramjit <bikramjit.garcha@outlook.com>
2018-11-04 19:21:45 -0800

    work in progress

--- q1.c
+++ q1.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void *findMin() {}
-void *findMax() {}
-void *findAvg() {}
+int findMin(void *input_ints);
+int findMax(void *input_ints);
+int findAvg(void *input_ints);
 	
 int main (int argc, char *argv[]){
 
@@ -12,21 +12,38 @@ int main (int argc, char *argv[]){
 		printf("Sorry sir/maam this program cannot execute due to the fact that there are not enough arguments.\nPlease I request that you next time enter in some numbers as an argument so this program can execute with joy.\nThanks! and have a wonderful day. :) \n");
 	}
 	int inputArr[argc - 1];
-	int min, max, avg;
+	void *min, *max, *avg;
+	int temp;
 	
 
 	for(int x = 1; x< argc; x++){
-		inputArr[x] = atoi(argv[x]);
-		printf("%d \n", inputArr[x]);
+		inputArr[x] = atoi(argv[x]);gcc
+		//printf("%d \n", inputArr[x]);
 	}
 
-	pthread_t threads[maxThread];
-	pthread_t threads[minThread];
-	pthread_t threads[avgThread];
-	pthread_create(&maxThread, NULL, findMax, NULL);
-	pthread_create(&minThread, NULL, findMin, inputArr);
-	pthread_create(&avgThread, NULL, findAvg, inputArr);
+	pthread_t maxThread, minThread, avgThread;
+	temp = pthread_create( &maxThread, NULL, (void *)findMax, (void *) inputArr);
+	temp = pthread_create( &minThread, NULL, (void *)findMin, (void *) inputArr);
+	temp = pthread_create( &avgThread, NULL, (void *)findAvg, (void *) inputArr);
 	
+	pthread_join(maxThread, &max);
+	pthread_join(minThread, &min);
+	pthread_join(avgThread, &avg);
+
+	printf("%d, %d, %d", min, max, avg);
 	
 	return 0;
+}
+
+int findMax(void *input_ints) {
+	printf("bik is gay");	
+	return 69;
+}
+
+int findMin(void *input_ints) {
+
+}
+
+int findAvg(void *input_ints) {
+
 }
